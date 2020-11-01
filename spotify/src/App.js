bafd02e45bb871bc40582255fc4e2475946dff9d
Imran Akbar <52159602+imran-prog@users.noreply.github.com>
2020-11-01 23:34:19 +0500

    created player component

--- spotify/src/App.js
+++ spotify/src/App.js
@@ -1,7 +1,11 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
 import Login from './Login';
+import Player from './Player';
 import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [token,setToken] = useState(null);
@@ -13,6 +17,12 @@ function App() {
 
     if (_token) {
       setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        console.log("User: ", user)
+      })
     }
 
     console.log("The token is: ", token);
@@ -22,7 +32,7 @@ function App() {
     <div className="app">
       {
         token ? (
-          <h1>I have logged in...</h1>
+          <Player />
         ) : (
           <Login />
         )
---
+++ spotify/src/Player.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function player() {
+    return (
+        <div>
+            <h1>Welcome To Spotify!!!</h1>
+        </div>
+    )
+}
+
+export default player
