1c0a2359d7c4d3268ff0255d2141cc519696f5ed
Imran Akbar <52159602+imran-prog@users.noreply.github.com>
2020-11-01 21:42:05 +0500

    Created Spotify

---
+++ spotify/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>We are building a spotify clone</h1>
+
+      {/* Spotify Logo */}
+      {/* Login With Spotify Button */}
+      
+    </div>
+  );
+}
+
+export default App;
