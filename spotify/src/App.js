b289e6cdf011d50d85156509c1266612fa8bed60
Imran Akbar <52159602+imran-prog@users.noreply.github.com>
2020-11-01 23:13:39 +0500

    Login Component Completed

--- spotify/src/App.js
+++ spotify/src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import Login from './Login';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+  const [token,setToken] = useState(null);
+
+  useEffect(() => {
+    const hash  = getTokenFromUrl();
+    window.location.hash = " ";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+
+    console.log("The token is: ", token);
+  }, []);
+
   return (
     <div className="app">
-      
-      <Login />
+      {
+        token ? (
+          <h1>I have logged in...</h1>
+        ) : (
+          <Login />
+        )
+        }
       
     </div>
   );
