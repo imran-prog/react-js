6007ea8f0587c2bd87c511806b1ceba5a58af959
imran-prog <infotainer04@gmail.com>
2022-01-11 14:38:14 +0500

    Created Grid Component

--- react-rmdb/src/components/Home.js
+++ react-rmdb/src/components/Home.js
@@ -5,6 +5,7 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config'
 
 //Components
 import HeroImage from './HeroImage'
+import Grid from './Grid'
 
 // Hook
 import { useHomeFetch } from '../hooks/useHomeFetch'
@@ -23,9 +24,12 @@ const Home = () => {
                     image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
                     title={state.results[0].original_title}
                     text={state.results[0].overview}
-                /> 
-                : null
-            }
+                /> : null}
+                <Grid header='Popular Movies'>
+                    {state.results.map(movie => (
+                        <div key={movie.id}>{movie.title}</div>
+                    ))}
+                </Grid>
         </>
     )
 }
