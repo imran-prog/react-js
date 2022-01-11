d0354256543c0f1964c115f5ae61228d6242c5ad
imran-prog <infotainer04@gmail.com>
2022-01-11 14:22:52 +0500

    Created Hero image Component

--- react-rmdb/src/components/Home.js
+++ react-rmdb/src/components/Home.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from 'react'
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config'
 
 //Components
+import HeroImage from './HeroImage'
 
 // Hook
 import { useHomeFetch } from '../hooks/useHomeFetch'
@@ -16,7 +17,16 @@ const Home = () => {
     console.log(state)
 
     return (
-        <div>Home Page Works</div>
+        <>
+            {state.results[0] ? 
+                <HeroImage 
+                    image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+                    title={state.results[0].original_title}
+                    text={state.results[0].overview}
+                /> 
+                : null
+            }
+        </>
     )
 }
 
