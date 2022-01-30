efdc2cc76a1c70ef19cdc1fde7011d8b1dc6e58f
imran-prog <infotainer04@gmail.com>
2022-01-30 21:51:53 +0500

    feat: :art: Changes Image design in MenuItem

--- crwn-mall/src/components/MenuItem.jsx
+++ crwn-mall/src/components/MenuItem.jsx
@@ -3,38 +3,26 @@ import styled from "styled-components";
 
 const MenuItem = ({ title, imageUrl, size }) => {
   return (
-    <Wrapper className={size} imageUrl={imageUrl}>
+    <Wrapper>
+      <Image className={size} imageUrl={imageUrl} />
       <Content>
-        <h1 className="title">{title}</h1>
+        <h1 className="title">{title.toUpperCase()}</h1>
         <span className="subtitle">SHOP NOW</span>
       </Content>
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div`
-  min-width: 30%;
-  height: 240px;
-  flex: 1 1 auto;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border: 1px solid black;
-  margin: 0 7.5px 15px;
+const Image = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
   background-image: url(${(props) => props.imageUrl});
   background-position: center;
   background-size: cover;
 
   &.large {
     height: 380px;
-  }
-
-  &:first-child {
-    margin-right: 7.5px;
-  }
-
-  &:last-child {
-    margin-left: 7.5px;
   }
 `;
 
@@ -46,18 +34,52 @@ const Content = styled.div`
   align-items: center;
   justify-content: center;
   border: 1px solid black;
+  background-color: white;
+  opacity: 0.7;
+  position: absolute;
 
   .title {
     font-weight: bold;
     margin-bottom: 6px;
     font-size: 22px;
     color: #4a4a4a;
-    text-transform: uppercase;
   }
 
   .subtitle {
     font-weight: lighter;
     font-size: 16px;
+  }
+`;
+
+const Wrapper = styled.div`
+  min-width: 30%;
+  height: 240px;
+  flex: 1 1 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid black;
+  margin: 0 7.5px 15px;
+
+  &:hover {
+    cursor: pointer;
+
+    & Image {
+      transform: scale(1.1);
+      transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
+    }
+
+    & Content {
+      opacity: 0.9;
+    }
+  }
+
+  &:first-child {
+    margin-right: 7.5px;
+  }
+
+  &:last-child {
+    margin-left: 7.5px;
   }
 `;
 
