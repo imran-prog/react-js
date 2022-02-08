66ec1491e8927f195833abcaa184e8cb1a8ede76
imran-prog <infotainer04@gmail.com>
2022-02-08 17:53:32 +0500

    feat: :fire: Added New shop data and made cart utility

--- crwn-mall/src/redux/cart/cart.reducer.js
+++ crwn-mall/src/redux/cart/cart.reducer.js
@@ -1,4 +1,5 @@
 import { cartActions } from "./cart.types";
+import { addItemToCart } from "./cart.utils";
 
 const INITIAL_STATE = {
   hidden: true,
@@ -15,7 +16,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     case cartActions.Add_Item:
       return {
         ...state,
-        cartItem: [...state.cartItem, action.payload],
+        cartItem: addItemToCart(state.cartItem, action.payload),
       };
     default:
       return state;
