d8187b9738f2b0068b97a91e074017ef78156220
imran-prog <infotainer04@gmail.com>
2022-02-11 16:26:50 +0500

    feat: :sparkles: Added redux persist
    Added redux persist to store cart data in local storage

--- crwn-mall/src/redux/rootReducer.js
+++ crwn-mall/src/redux/rootReducer.js
@@ -2,8 +2,19 @@ import { combineReducers } from "redux";
 // Reducers
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
+// Persist
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-export default combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  whilelist: ["cart"],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
--- crwn-mall/src/redux/store.js
+++ crwn-mall/src/redux/store.js
@@ -3,6 +3,8 @@ import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 // Root Reducer
 import rootReducer from "./rootReducer";
+// Redux Persist
+import { persistStore } from "redux-persist";
 
 // Middleware
 const middleware = [logger];
@@ -10,5 +12,8 @@ const middleware = [logger];
 // Store
 const store = createStore(rootReducer, applyMiddleware(...middleware));
 
+// Persistor
+const persistor = persistStore(store);
+
 // Export store
-export default store;
+export { store, persistor };
